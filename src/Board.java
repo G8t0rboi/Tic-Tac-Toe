93b3a4ef8c83a9dcd074508b8478389062a37fd7
G8t0rboi <g8t0rboispam@gmail.com>
2019-07-17 12:08:39 -0400

    Started Multiplayer Support

--- src/Board.java
+++ src/Board.java
@@ -2,14 +2,40 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.net.*;
+import java.io.*;
+
 
 public class Board {
     private int turn = 1;
-    private int xCount = 0;
-    private int oCount = 0;
     private JButton[][] bo = new JButton[3][3];
-    private int[][] scores =  new int[3][3];
+    private int[][] scores = new int[3][3];
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true );
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+        if ( "hello server".equals(greeting)) {
+            out.println("hello client");
+        }
+        else {
+            out.println("unrecognized greeting");
+        }
+
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
 
     public static void main(String[] args) {
         Board boi = new Board();
@@ -17,44 +43,59 @@ public class Board {
     }
 
     public void setUp() {
-        GridLayout grid = new GridLayout(3,3);
+        GridLayout grid = new GridLayout(3, 3);
         JFrame frame = new JFrame("Boi");
         frame.setLayout(grid);
-        for ( int i = 0; i < bo.length; i++ ) {
-            for ( int a = 0; a < bo[0].length; a++ ) {
+        for (int i = 0; i < bo.length; i++) {
+            for (int a = 0; a < bo[0].length; a++) {
                 bo[i][a] = new JButton();
                 frame.add(bo[i][a]);
             }
         }
-        for ( JButton[] butt : bo ) {
-            for ( JButton boot : butt ) {
+        for (int a = 0; a < bo.length; a++) {
+            for (int b = 0; b < bo[a].length; b++) {
+                JButton boot = bo[a][b];
+                int row = a;
+                int col = b;
                 boot.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        if ( checkWin() )  {
-
-                        }
-                        if ( boot.getText().equals("") ) {
-                            boot.setText( checkTurn() );
+                        if (boot.getText().equals("")) {
+                            boot.setText(checkTurn(row, col));
                         }
                     }
                 });
             }
         }
-        frame.setSize(500,500);
+        frame.setSize(500, 500);
         frame.setVisible(true);
+        for(;;) {
+            if (checkWin()) {
+                if ( turn % 2 != 0 ) {
+                    System.out.println("Os won the game!");
+                }
+                else {
+                    System.out.println("Xs won the game!");
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.exit(1);
+            }
+        }
     }
 
-    public String checkTurn() {
+    public String checkTurn(int row, int col) {
 
-        if ( turn % 2 != 0 ) {
+        if (turn % 2 != 0) {
             turn++;
-            xCount++;
+            scores[row][col]++;
             return "X";
-        }
-        else {
+        } else {
             turn++;
-            oCount--;
+            scores[row][col]--;
             return "O";
         }
     }
@@ -68,32 +109,27 @@ public class Board {
         int lstcol = scores[0][2] + scores[1][2] + scores[2][2];
         int forwarddiag = scores[2][0] + scores[1][1] + scores[0][2];
         int backwarddiag = scores[0][0] + scores[1][1] + scores[2][2];
-        if ( toprow == 3 || toprow == -3) {
+        if (toprow == 3 || toprow == -3) {
             return true;
-        }
-        else if ( midrow == 3 || midrow == -3) {
+        } else if (midrow == 3 || midrow == -3) {
             return true;
-        }
-        else if ( botrow == 3 || botrow == -3) {
+        } else if (botrow == 3 || botrow == -3) {
             return true;
-        }
-        else if ( fstcol == 3 || fstcol == -3) {
+        } else if (fstcol == 3 || fstcol == -3) {
             return true;
-        }
-        else if ( sndcol == 3 || sndcol == -3) {
+        } else if (sndcol == 3 || sndcol == -3) {
             return true;
-        }
-        else if ( lstcol == 3 || lstcol == -3) {
+        } else if (lstcol == 3 || lstcol == -3) {
             return true;
-        }
-        else if ( forwarddiag == 3 || forwarddiag == -3) {
+        } else if (forwarddiag == 3 || forwarddiag == -3) {
             return true;
-        }
-        else if ( backwarddiag == 3 || backwarddiag == -3) {
+        } else if (backwarddiag == 3 || backwarddiag == -3) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
+
+
     }
+
 }
